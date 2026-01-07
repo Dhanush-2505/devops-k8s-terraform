2bc5b8cd5c5e41209100f7d63ecd7be3dc308206
Dhanush-2005 <dhanushnagarajan1976@gmail.com>
2026-01-07 18:27:38 +0530

    Clean repo: ignore Terraform generated files

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "eks" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = [
+      "us-east-1a",
+      "us-east-1b",
+      "us-east-1c",
+      "us-east-1d",
+      "us-east-1f"
+    ]
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.eks.ids
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 2
+      instance_types = ["t3.small"]
+    }
+  }
+}
+
